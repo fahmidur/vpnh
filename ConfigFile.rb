9ad042de8180840e17aeaff4b0c8f051d8d144c4
Syed Reza <s.f.reza@gmail.com>
2019-09-26 21:03:11 -0700

    cleanup

---
+++ ConfigFile.rb
@@ -0,0 +1,50 @@
+# A class to represent JSON persisted
+# configuration files.
+class ConfigFile
+  require 'json'
+  def initialize(file_path)
+    @file_path = file_path
+    raise 'expecting file_path' unless @file_path
+    @data_read = {}
+    @data = {}
+    data_read
+    data_write if changed?
+  end
+
+  def get(key)
+    @data[key.to_s]
+  end
+
+  def set(key, val)
+    @data[key.to_s] = val
+    data_write if changed?
+  end
+
+  def data_read
+    return unless File.exists?(@file_path)
+    gdata = {}
+    odata = JSON.parse(IO.read(@file_path))
+    odata.each do |k, v|
+      next if k[0] == '_'
+      gdata[k] = v
+    end
+    @data_read = gdata.clone
+    @data = gdata.clone
+  end
+
+  def data_write
+    $logger.info "VpnhConfig. data_write ..."
+    dirname = File.dirname(@file_path)
+    FileUtils.mkdir_p(dirname)
+    data = @data.clone
+    data['_write_ts'] = Time.now.to_i
+    json_str = JSON.pretty_generate(data)
+    IO.write(@file_path, json_str+"\n")
+    data_read
+  end
+
+  def changed?
+    @data_read != @data || !File.exists?(@file_path)
+  end
+
+end
