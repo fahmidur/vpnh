ebb648592ffdde391d39c2bca6b56e3883d9a75a
Syed Reza <s.f.reza@gmail.com>
2019-09-28 00:49:19 -0700

    user_add

--- Util.rb
+++ Util.rb
@@ -9,9 +9,35 @@ module Util
     Set.new(IO.read('/etc/passwd').split("\n").map {|e| e.split(':')[0] })
   end
 
-  def self.user_exists?(name)
+  def self.user_exists?(username)
     users = get_all_users
-    users.member?(name)
+    users.member?(username)
+  end
+
+  def self.user_add(username)
+    unless Util.sys_write_ok?
+      puts "user_add. sys_write denied"
+      return nil
+    end
+    raise ArgumentError.new('expecting username') unless username
+    raise ArgumentError.new('expecting string username') unless username.is_a?(String)
+    raise ArgumentError.new('invalid username') unless username =~ /^\w+$/
+    users = Util.get_all_users
+    return nil if users.member?(username)
+    puts "user_add. adding user username=#{username}"
+    res = `useradd -m #{username}`
+    puts "user_add. useradd: #{res}"
+    return username
+  end
+
+  def self.get_machine_id
+    IO.read('/etc/machine-id').strip
+  end
+
+  def self.sys_write_ok?
+    return false if ENV['VPNH_NO_WRITE'] == '1'
+    return false if Util.get_machine_id == 'b4fa03f9870a490183fc34a79efe6513'
+    return true
   end
 
   def self.get_all_ifaces
