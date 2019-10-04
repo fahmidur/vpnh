77af9123bb5da2a037d5d3a4ff5937f075249c6e
Syed Reza <s.f.reza@gmail.com>
2019-10-04 11:35:54 -0700

    comment-out co-ensure

--- VpnhMaster.rb
+++ VpnhMaster.rb
@@ -12,7 +12,6 @@ class VpnhMaster
 
   def initialize
     @config = VpnhConfig.new(con_path)
-    #co_ensure
   end
 
   def server_daemon_ensure
@@ -54,22 +53,22 @@ class VpnhMaster
     IO.read(path).strip.split('.')[0..2].map(&:to_i)
   end
 
-  def co_ensure
-    if co_path == __dir__
-      puts "co_ensure. skipped. called from co"
-      return
-    end
-    Dir.mkdir_p(co_path) unless Dir.exists?(co_path)
-    co_version = semver_read(File.join(co_path, 'VERSION'))
-    my_version = semver_read(File.join(__dir__, 'VERSION'))
-    if co_version == my_version
-      puts "co_ensure. skipped. version is same"
-      return
-    end
-    puts "co_ensure. copying co from here..."
-    FileUtils.cp_r(File.join(__dir__, '.'), co_path)
-    puts "co_ensure. copyzzz co from here... DONE"
-  end
+  #def co_ensure
+    #if co_path == __dir__
+      #puts "co_ensure. skipped. called from co"
+      #return
+    #end
+    #Dir.mkdir_p(co_path) unless Dir.exists?(co_path)
+    #co_version = semver_read(File.join(co_path, 'VERSION'))
+    #my_version = semver_read(File.join(__dir__, 'VERSION'))
+    #if co_version == my_version
+      #puts "co_ensure. skipped. version is same"
+      #return
+    #end
+    #puts "co_ensure. copying co from here..."
+    #FileUtils.cp_r(File.join(__dir__, '.'), co_path)
+    #puts "co_ensure. copyzzz co from here... DONE"
+  #end
 
   def config
     @config
